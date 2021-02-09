b26693197a13677d77e5e68c53689cdde18c7ffc
msharsh <mykola.sharshatkin@gmail.com>
2021-02-09 18:28:24 +0200

    Adding documentation

---
+++ puzzle.py
@@ -0,0 +1,34 @@
+"""
+This module contain functions which are cheking whether the given board
+fits the rules.
+Git repository: https://github.com/msharsh/puzzle.git
+"""
+def check_rows(board: list) -> bool:
+    """
+    Checks if there are identical numbers in every row in the board.
+Returns True if not, else False.
+    """
+    pass
+
+
+def check_columns(board: list) -> bool:
+    """
+    Checks if there are identical numbers in every column in the board.
+Returns True if not, else False.
+    """
+    pass
+
+
+def check_color(board: list) -> bool:
+    """
+    Checks if there are identical numbers in every color row in the board.
+Returns True if not, else False.
+    """
+    pass
+
+
+def validate_board(board: list) -> bool:
+    """
+    Checks if board fits the rules. If fits returns True, else False.
+    """
+    pass
